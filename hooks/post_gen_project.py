163268da1c3d2ea9dc9c15a5a94afb2224b6f4e6
Martin Migasiewicz <616250+martinm82@users.noreply.github.com>
2019-08-01 11:47:23 +0200

    fixup: delete pyproject.toml file if it is empty

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,13 +4,24 @@ import shutil
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
+
+def template_file(filename):
+    return os.path.join(PROJECT_DIRECTORY, filename)
+
+
+def file_size(filename):
+    return os.stat(template_file(filename)).st_size
+
+
 def remove_file(filepath):
-    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+    os.remove(template_file(filepath))
+
 
 def remove_dir(path):
-    shutil.rmtree(os.path.join(PROJECT_DIRECTORY, path))
+    shutil.rmtree(template_file(path))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     if "{{ cookiecutter.vs }}".lower() == "bitbucket":
         remove_dir(".github")
@@ -20,3 +31,6 @@ if __name__ == '__main__':
 
     if "{{ cookiecutter.ci }}".lower() == "jenkins":
         remove_file("azure-pipelines.yml")
+
+    if file_size("pyproject.toml") == 0:
+        remove_file("pyproject.toml")
